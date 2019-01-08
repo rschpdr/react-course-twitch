d9df6fc9895abfbfa746de014d188fa798d6a717
Pedro Resch <pedro.resch@gmail.com>
2019-01-07 23:07:12 -0200

    lesson 210

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from '../components/streams/StreamCreate';
 import StreamDelete from '../components/streams/StreamDelete';
 import StreamEdit from '../components/streams/StreamEdit';
 import StreamList from '../components/streams/StreamList';
 import StreamShow from '../components/streams/StreamShow';
+import Header from './Header';
 
 
 const App = () => {
-    return (
+  return (
+    <div className="ui container">
+      <BrowserRouter>
         <div>
-            <BrowserRouter>
-                <div>
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
-                </div>
-            </BrowserRouter>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
-    );
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
